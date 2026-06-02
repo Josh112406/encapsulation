521559c635d966b663fb3e71c6bc720bb040bd52
Josh112406 <quibaljosh@gmail.com>
2026-06-03 01:10:01 +0800

    initialize constructor and declare private variables

--- Fan/fan.py
+++ Fan/fan.py
@@ -2,3 +2,8 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+    def __init__(self, speed:int = SLOW, radius: float = 5, color: str = "blue", state: bool = False):
+        self.__speed: int = speed
+        self.__state: bool = state
+        self.__radius: float = radius
+        self.__color: str = color
