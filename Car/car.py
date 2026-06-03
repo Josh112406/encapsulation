d2b2ca063ec350a6ce93d5737bedf0cae00cf792
Josh112406 <quibaljosh@gmail.com>
2026-06-04 00:41:05 +0800

    define methods

---
+++ Car/car.py
@@ -0,0 +1,13 @@
+class Car:
+    def __init__(self):
+        pass
+    
+    def accelerate(self):
+        pass
+    
+    def brake(self):
+        pass
+    
+    def get_speed(self):
+        pass
+    
