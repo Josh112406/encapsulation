6cb393e9ea95f5e7f7f8fc17dccbdae4e6754b67
Josh112406 <quibaljosh@gmail.com>
2026-06-04 01:38:43 +0800

    crease Pet class and declare properties and methods

---
+++ Pet/pet.py
@@ -0,0 +1,23 @@
+class Pet:
+    def __init__(self):
+        self. __name 
+        self.__animal_type
+        self.__age
+        
+    def set_name(self):
+        pass
+    
+    def set_animal_type(self):
+        pass
+    
+    def set_age(self):
+        pass
+    
+    def get_name(self):
+        pass
+    
+    def get_animal_type():
+        pass
+    
+    def get_age(self):
+        pass
