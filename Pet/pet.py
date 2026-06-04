668a6032ebc7d306a4abbdbd40296f8d50b78274
Josh112406 <quibaljosh@gmail.com>
2026-06-04 12:09:58 +0800

    finalized Fan class and define getters and setters

--- Pet/pet.py
+++ Pet/pet.py
@@ -3,21 +3,23 @@ class Pet:
         self. __name = ""
         self.__animal_type = ""
         self.__age = 0
-        
+    
+    #setters    
     def set_name(self, name):
         self.__name = name
     
     def set_animal_type(self, animal_type):
         self.__animal_type = animal_type
     
-    def set_age(self):
-        pass
+    def set_age(self, age):
+        self.__age = age
     
+    #getters
     def get_name(self):
-        pass
+        return self.__name
     
-    def get_animal_type():
-        pass
+    def get_animal_type(self):
+        return self.__animal_type
     
     def get_age(self):
-        pass
+        return self.__age
